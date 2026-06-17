97c5c6f44c64f5a4983892d2af4ce09c1bb6021b
just-ketan <ketanbammidi@gmail.com>
2026-06-17 16:58:20 +0530

    push

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -8,11 +8,11 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-import { Route as rootRouteImport } from './routes/__root.tsx'
-import { Route as SettingsRouteImport } from './routes/settings.tsx'
-import { Route as HistoryRouteImport } from './routes/history.tsx'
-import { Route as GeneratorRouteImport } from './routes/generator.tsx'
-import { Route as BrandsRouteImport } from './routes/brands.tsx'
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as SettingsRouteImport } from './routes/settings'
+import { Route as HistoryRouteImport } from './routes/history'
+import { Route as GeneratorRouteImport } from './routes/generator'
+import { Route as BrandsRouteImport } from './routes/brands'
 import { Route as IndexRouteImport } from './routes/index'
 
 const SettingsRoute = SettingsRouteImport.update({
